9eb65944a743ae4b4d198fa04c6af416faa635d7
ravkovskiy <kos1992@mail.ru>
2016-06-16 12:16:46 +0300

    Проект перенесен из lite-server in express

--- client/app/bootstrap.ts
+++ client/app/bootstrap.ts
@@ -3,10 +3,15 @@ import {provide}    from 'angular2/core'
 import {CORE_DIRECTIVES} from 'angular2/common'
 import {ROUTER_BINDINGS, ROUTER_PROVIDERS, LocationStrategy, PathLocationStrategy} from 'angular2/router'
 
+import {PriceService} from "./components/price/price.service";
+import {PortfolioService} from "./components/portfolio/portfolio.service";
+
 import {AppComponent} from './components/app/AppComponent'
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     CORE_DIRECTIVES,
+    PriceService,
+    PortfolioService,
     provide(LocationStrategy, {useClass: PathLocationStrategy})
 ]);
 
