5297cbe3a727a45048230cf4278f2ebf4418db53
ravkovskiy <kos1992@mail.ru>
2016-06-30 15:30:43 +0300

    added error handling

--- client/app/createDB.js
+++ client/app/createDB.js
@@ -1,15 +1,42 @@
-var User = require('./models/user').User;
+var mongoose = require('./mongoose');
+var async = require('async');
 
-var user = new User({
-    username: "Tester3",
-    password: "secret"
-});
-user.save(function(err, user, affected) {
-    if(err) throw err;
-
-    User.findOne({username: "Tester"}, function(err, tester) {
-        if(err) throw err;
-        console.log(tester);
-        
-    })
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createUsers
+], function(err) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255 : 0);
 })
+function open(callback) {
+    mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+    require('./models/user');
+
+    async.each(Object.keys(mongoose.models), function(modelName, callback) {
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createUsers(callback) {
+    var users = [
+        {username: 'Вася', password: 'supervasya'},
+        {username: 'Петя', password: '123'},
+        {username: 'admin', password: 'thetruehero'}     
+    ];
+    async.each(users, function(userData, callback) {
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
+    }, callback);
+}
+
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express = require('express');
 import path = require('path');
 var config = require('./app/config');
+var HttpError = require('./app/error').HttpError;
+var ObjectID = require('mongodb').ObjectID;
 var port: number = process.env.PORT || config.get('port');
 var app = express();
 
@@ -13,8 +15,44 @@ var renderIndex = (req: express.Request, res: express.Response) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 }
 
-app.get('/*', renderIndex);
+app.use(require('./app/middleware/sendHttpError'));
 
+//app.get('/*', renderIndex);
+
+var User = require('./app/models/user').User;
+app.get('/users', function(req, res, next) {
+    User.find({}, function(err, users) {
+        if(err) return next(err);
+        res.json(users);
+    })
+});
+app.get('/user/:id', function(req, res, next) {
+
+    try {
+        var id = new ObjectID(req.params.id);
+    } catch(e) {
+        return next(404);
+    }
+    User.findById(id, function(err, user) {
+        if(err) return next(err);
+        if(!user) {
+            next(new HttpError(404, 'User not found'));
+        } else
+        res.json(user);
+    })
+});
+
+app.use(function(err, req, res, next) {
+    if(typeof err == 'number') {
+        err = new HttpError(err);
+    } 
+    if(err instanceof HttpError) {
+        res.sendHttpError(err);
+    } else {
+            err = new HttpError(500);
+            res.sendHttpError(err);
+    }
+});
 var server = app.listen(port, function() {
     var host = server.address().address;
     var port = server.address().port;
